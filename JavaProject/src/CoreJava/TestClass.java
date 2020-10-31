ba15954d63798e59a38f95beabcd055425f368b8
kranthi kumar vadlamudi <kranthiv1992@gmail.com>
2020-10-31 19:07:57 -0400

    added new variable j

--- JavaProject/src/CoreJava/TestClass.java
+++ JavaProject/src/CoreJava/TestClass.java
@@ -5,7 +5,10 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  int i=10;
+ int j=20;
  System.out.println(i);
+ System.out.println(j);
+ 
 	}
 
 }
